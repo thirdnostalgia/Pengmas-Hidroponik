5e3274137ec5c54c5688955a77a73d5dc1b54fe9
abdullah.al.masyhur-2020@ftmm.unair.ac.id <abdullah.al.masyhur-2020@ftmm.unair.ac.id>
2023-02-24 23:16:33 +0700

    Simulasi Lcd dengan serial

--- Kodingan LCD/Kodingan LCD.ino
+++ Kodingan LCD/Kodingan LCD.ino
@@ -1,44 +1,119 @@
-
 #include <LiquidCrystal_I2C.h>
 
 // Set the LCD address to 0x27 for a 16 chars and 2 line display
 LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+uint8_t arrow[8] = {0x00, 0x04 ,0x06, 0x1f, 0x06, 0x04, 0x00};
+String input;
 
 void setup()
 {
 	// initialize the LCD
 	lcd.begin();
   Serial.begin(9600);
-
-	
+  lcd.createChar(0, arrow); //logo panah
 }
 
 void loop()
 {
+  awal :
+  lcd.clear();
 	lcd.setCursor(0, 0);
   lcd.print("HIDROPONIK--TRKB");
   lcd.setCursor(0, 1);
-  lcd.print("SET       STATUS");
+  lcd.write(0);
+  lcd.setCursor(1, 1);
+  lcd.print("SET      STATUS");
   while(Serial.available() == 0){}
   
-  String input = Serial.readString();
+  input = Serial.readString();
   input.trim();
-  lcd.clear();
-
-  if(input == "set"){
-   lcd.setCursor(0, 0);
-    lcd.print("jeda nutrisi (s)");
-    lcd.setCursor(0, 1);
-    lcd.print("5   10   15   20");
-    while(Serial.available() == 0){}
-    String input = Serial.readString();
-    input.trim();
+  
+  if(input == "kanan"){
     lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("HIDROPONIK--TRKB");
+    lcd.setCursor(1, 1);
+    lcd.print("SET      STATUS");
+    lcd.setCursor(9, 1);
+    lcd.write(0);
+    while(Serial.available() == 0){}
     
-    if(input == "5"){
+    input = Serial.readString();
+    input.trim();
+
+    if(input == "ok"){
+      lcd.clear();
       lcd.setCursor(0, 0);
-      lcd.print("On Progres...");
-       while(Serial.available() == 0){}
+      lcd.print("STAUTUS");
+      delay(2000);
+      goto awal;
+      
     }
+    if(input == "kanan"){
+      goto awal;
+    }
+  }
+  
+  if(input == "ok"){
+    set :
+    lcd.setCursor(0, 0);
+    lcd.print("Durasi (detik) ");
+    lcd.setCursor(0, 1);
+    lcd.write(0);
+    lcd.setCursor(1, 1);
+    lcd.print("5     10     15");
+    while(Serial.available() == 0){}
+    
+    input = Serial.readString();
+    input.trim();
+    Serial.println(input);
+
+    if (input == "kanan"){
+      lcd.clear();
+      Serial.println("dua");
+      lcd.setCursor(0, 0);
+      lcd.print("Durasi (detik) ");
+      lcd.setCursor(1, 1);
+      lcd.print("5     10     15");
+      lcd.setCursor(6, 1);
+      lcd.write(0);
+      while(Serial.available() == 0){}
+      
+      input = Serial.readString();
+      input.trim();
+     
+      if (input == "kanan"){
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("Durasi (detik) ");
+        lcd.setCursor(1, 1);
+        lcd.print("5     10     15");
+        lcd.setCursor(13, 1);
+        lcd.write(0);
+        while(Serial.available() == 0){}
+
+        input = Serial.readString();
+        input.trim();
+
+        if(input == "kanan"){
+          lcd.clear();
+          goto set;
+        }
+      }
+    }
+  }
+
+  if(input == "ok"){
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("NUTRISI ON DUTY ");
+    delay(5000);
+    goto awal;
+  }
+  
+  if(input == "back"){
+    lcd.clear();
+    goto awal;
   }
 }
